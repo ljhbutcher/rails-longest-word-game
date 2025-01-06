310c8004d6998f0c4fb40fc1b07aef36d282fc4a
Liam Butcher <liam_butcher@hotmail.co.uk>
2025-01-06 18:02:09 +0000

    rails new

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -1,5 +1,4 @@
-require 'open-uri'
-require 'json'
+require "open-uri"
 
 class GamesController < ApplicationController
   def new
@@ -7,33 +6,24 @@ class GamesController < ApplicationController
   end
 
   def score
-    @letters = params[:letters].split(' ')
+    @letters = params[:letters].split
     @word = params[:word].upcase
-
-    if valid_word?(@word, @letters)
-      if english_word?(@word)
-        @message = "Well done! #{@word} is a valid word."
-        @score = @word.length
-      else
-        @message = "#{@word} is not an English word."
-        @score = 0
-      end
-    else
-      @message = "#{@word} cannot be made from the grid."
-      @score = 0
-    end
-  end
-
-  def english_word?(word)
-    response = URI.parse("https://dictionary.lewagon.com/#{word}")
-    json = JSON.parse(response.read)
-    return json['found']
+    @valid_word = valid_word?(@word, @letters)
+    @english_word = english_word?(@word)
   end
 
   private
 
   def valid_word?(word, letters)
-    word.chars.all? { |char| word.count(char) <= letters.count(char) }
+    word.chars.all? do |letter|
+      word.count(letter) <= letters.count(letter)
+    end
+  end
+
+  def english_word?(word)
+    response = URI.open("https://dictionary.lewagon.com/#{word}")
+    json = JSON.parse(response.read)
+    json['found']
   end
 
 end
